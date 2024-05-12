df233c1fb07d1897c185cc3afc411b89747ec02b
Eduardo Henrique <eduardoehg@outlook.com>
2024-05-12 15:11:43 -0300

    ajustes no tempo

--- raspagem.py
+++ raspagem.py
@@ -11,7 +11,7 @@ from time import sleep
 
 def esperar(navegador, elemento):
     WebDriverWait(navegador, 30).until(EC.element_to_be_clickable((By.XPATH, elemento)))
-    sleep(.5)
+    sleep(1)
 
 
 class RasparDados:
@@ -39,31 +39,14 @@ class RasparDados:
         entrar = self.navegador.find_element(By.XPATH, '//*[@id="kc-content-wrapper"]/react-login/div/div/div[1]/div[1]/div[1]/form/div[3]/button')
         entrar.click()
 
-        sleep(2)
+        esperar(self.navegador, elemento='//*[@id="main-menu"]/div[2]/div[1]/div[1]/div[1]')
         self.navegador.get(f'https://erp.tiny.com.br/anuncios?idEcommerce={self.id_ecommerce}')
 
-        self.navegar()
-
-    def navegar(self):
         self._pagina_anuncio()
-
-        while True:
-            esperar(self.navegador, elemento='//*[@id="tabelaListagem"]/thead/tr/th[1]/span')
-            pag = self.navegador.page_source
-            extra = BeautifulSoup(pag, 'html.parser')
-            pagination_ul = extra.find('ul', class_='pagination hidden-xs')
-            try:
-                pagination_ul.find('li', class_='pnext')
-                sleep(1.5)
-                botao_avancar = self.navegador.find_elements(By.CLASS_NAME, 'pnext')
-                botao_avancar[1].click()
-                self._pagina_anuncio()
-            except AttributeError:
-                self.navegador.quit()
-                break
 
     def _pagina_anuncio(self):
         esperar(self.navegador, elemento='//*[@id="tabelaListagem"]/thead/tr/th[1]/span')
+        sleep(5)
         pagina = self.navegador.page_source
         site = BeautifulSoup(pagina, 'html.parser')
         pesquisa = site.find_all('tr', idproduto='0', attrs={'idanuncio': True})
@@ -73,27 +56,26 @@ class RasparDados:
             print(id_anuncio)
             lista_anuncios.append(id_anuncio)
 
-        self._anuncio_individual(lista_anuncios)
-
-        esperar(self.navegador, elemento='//*[@id="page-wrapper"]/div[4]/div/div[1]/ol/li[1]/a')
-        self.navegador.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[4]/div/div[1]/ol/li[1]/a').click()
+        if lista_anuncios:
+            self._anuncio_individual(lista_anuncios)
+        else:
+            self._proxima_pagina()
 
     def _anuncio_individual(self, lista_anuncios):
         for anuncio in lista_anuncios:
-            sleep(3)
+            sleep(1.5)
             self.navegador.get(f'https://erp.tiny.com.br/anuncios?idEcommerce={self.id_ecommerce}#edit/{anuncio}')
-            esperar(self.navegador, elemento='//*[@id="link-atributos"]')
-            pagina_anuncio = self.navegador.page_source
-            informacoes_anuncio = BeautifulSoup(pagina_anuncio, 'html.parser')
+            esperar(self.navegador, elemento='//*[@id="section-dados-imagens"]')
+            sleep(5)
+            cadastro_anuncio = self.navegador.page_source
+            informacoes_anuncio = BeautifulSoup(cadastro_anuncio, 'html.parser')
             ficha_tecnica = informacoes_anuncio.find('div', class_='atributos-recomendados')
 
             if ficha_tecnica:
                 input_element = ficha_tecnica.find('input', {'value': 'SELLER_SKU'})
-
                 if input_element:
                     siblings = input_element.find_next_siblings()
                     p_tags = [BeautifulSoup(str(sibling), 'html.parser').find('p', class_='form-control-static viewing-input') for sibling in siblings]
-
                     if p_tags[1]:
                         sku = p_tags[1].get_text(strip=True)
                         print(sku)
@@ -103,3 +85,23 @@ class RasparDados:
                             Vinculo(self.navegador, sku)
                         else:
                             print('nao vou vincular')
+        esperar(self.navegador, elemento='//*[@id="page-wrapper"]/div[4]/div/div[1]/ol/li[1]/a')
+        sleep(5)
+        self.navegador.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[4]/div/div[1]/ol/li[1]/a').click()
+        self._proxima_pagina()
+
+    def _proxima_pagina(self):
+        esperar(self.navegador, elemento='//*[@id="tabelaListagem"]/thead/tr/th[1]/span')
+        pag = self.navegador.page_source
+        extra = BeautifulSoup(pag, 'html.parser')
+        pagination_ul = extra.find('ul', class_='pagination hidden-xs')
+        try:
+            pagination_ul.find('li', class_='pnext')
+            sleep(1.5)
+            botao_avancar = self.navegador.find_elements(By.CLASS_NAME, 'pnext')
+            botao_avancar[1].click()
+            esperar(self.navegador, elemento='//*[@id="tabelaListagem"]/thead/tr/th[1]/span')
+            sleep(2)
+            self._pagina_anuncio()
+        except AttributeError:
+            self.navegador.quit()
--- vincular.py
+++ vincular.py
@@ -1,6 +1,13 @@
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def esperar(navegador, elemento):
+    WebDriverWait(navegador, 30).until(EC.element_to_be_clickable((By.XPATH, elemento)))
+    sleep(1)
 
 
 class Vinculo:
@@ -13,26 +20,32 @@ class Vinculo:
 
     def relacionar(self):
         sleep(1)
+        esperar(self.navegador, elemento='//*[@id="page-wrapper"]/div[4]/div/div[1]/div/div[2]/button')
         botao_acoes = self.navegador.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[4]/div/div[1]/div/div[2]/button')
         botao_acoes.click()
 
         sleep(1)
+        esperar(self.navegador, elemento='//*[@id="im_12"]/a')
         botao_relacionar_anuncios = self.navegador.find_element(By.XPATH, '//*[@id="im_12"]/a')
         botao_relacionar_anuncios.click()
 
         sleep(1)
+        esperar(self.navegador, elemento='//*[@id="bs-modal"]/div/div/div/div[2]/div[2]/div[1]/div/div/input')
         barra_pesquisa = self.navegador.find_element(By.XPATH, '//*[@id="bs-modal"]/div/div/div/div[2]/div[2]/div[1]/div/div/input')
         barra_pesquisa.send_keys(self.sku)
         barra_pesquisa.send_keys(Keys.ENTER)
 
         sleep(1)
+        esperar(self.navegador, elemento='//*[@id="bs-modal"]/div/div/div/div[2]/div[2]/div[2]/table/tbody/tr/td[4]')
         produto = self.navegador.find_element(By.XPATH, '//*[@id="bs-modal"]/div/div/div/div[2]/div[2]/div[2]/table/tbody/tr/td[4]')
         produto.click()
 
         sleep(1)
+        esperar(self.navegador, elemento='//*[@id="bs-modal"]/div/div/div/div[3]/button[1]')
         botao_continuar = self.navegador.find_element(By.XPATH, '//*[@id="bs-modal"]/div/div/div/div[3]/button[1]')
         botao_continuar.click()
 
         sleep(1)
+        esperar(self.navegador, elemento='//*[@id="bs-modal"]/div/div/div/div[3]/button[2]')
         botao_relacionar = self.navegador.find_element(By.XPATH, '//*[@id="bs-modal"]/div/div/div/div[3]/button[2]')
         botao_relacionar.click()
