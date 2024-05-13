92e668e19d80201ff542fefab453b4984e1e3ef7
Eduardo Henrique <eduardoehg@outlook.com>
2024-05-13 12:23:30 -0300

    atualizando o excluir.py

--- excluir.py
+++ excluir.py
@@ -3,37 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
 from bs4 import BeautifulSoup
 from time import sleep
 
-
-def excluir_anuncios(nav):
-    sleep(1)
-    tempo = 5
-    wait = WebDriverWait(navegador, tempo)
-    elemento = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[1]/div/div[1]/button')))
-
-    botao = nav.find_element(By.XPATH, '//*[@id="tabelaListagem"]/thead/tr/th[1]/span')
-    botao.click()
-    sleep(1)
-
-    botao_mais_acoes = nav.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[4]/div[1]/div[3]/div[2]/button')
-    botao_mais_acoes.click()
-    sleep(1)
-
-    botao_excluir = nav.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[4]/div[1]/div[3]/div[2]/div/ul/li[6]/a')
-    botao_excluir.click()
-    sleep(1)
-
-    botao_confirmar = nav.find_element(By.XPATH, '//*[@id="bs-modal-ui-popup"]/div/div/div/div[3]/button[1]')
-    botao_confirmar.click()
-
-
-########## ADICIONAR OS DADOS DA CONTA #####################
 email = ''
 senha = ''
 id_ecommerce = ''
-############################################################
 
 navegador = webdriver.Chrome()
 navegador.get('https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/auth?client_id=tiny-webapp&redirect_uri=https://erp.tiny.com.br/login&scope=openid&response_type=code')
@@ -52,8 +28,38 @@ sleep(5)
 navegador.get(f'https://erp.tiny.com.br/anuncios?idEcommerce={id_ecommerce}')
 sleep(5)
 
+
+def clicar_elemento_com_espera(elemento):
+    try:
+        elemento_clicavel = WebDriverWait(navegador, 10).until(EC.element_to_be_clickable(elemento))
+        elemento_clicavel.click()
+    except TimeoutException:
+        print("Timeout ao esperar pelo elemento clicável.")
+    except ElementClickInterceptedException:
+        print("Elemento interceptado ao tentar clicar.")
+
+
+def excluir_anuncios(nav):
+    sleep(1)
+    while len(navegador.find_elements(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[1]/div/div[1]/button')) < 1:
+        sleep(5)
+
+    clicar_elemento_com_espera((By.XPATH, '//*[@id="tabelaListagem"]/thead/tr/th[1]/span'))
+    sleep(1)
+
+    clicar_elemento_com_espera((By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[4]/div[1]/div[3]/div[2]/button'))
+    sleep(1)
+
+    clicar_elemento_com_espera((By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[4]/div[1]/div[3]/div[2]/div/ul/li[6]/a'))
+    sleep(1)
+
+    clicar_elemento_com_espera((By.XPATH, '//*[@id="bs-modal-ui-popup"]/div/div/div/div[3]/button[1]'))
+
+    while len(navegador.find_elements(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[1]/div/div[1]/button')) < 1:
+        sleep(5)
+
 botao_filtros = navegador.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[3]/ul/li[1]/a')
-botao_filtros.click()
+clicar_elemento_com_espera((By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[3]/ul/li[1]/a'))
 sleep(1)
 
 situacao = navegador.find_element(By.XPATH, '//*[@id="filtroRelacionados"]')
@@ -62,13 +68,11 @@ situacao.send_keys(Keys.ENTER)
 sleep(1)
 
 botao_aplicar = navegador.find_element(By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/div[5]/button[1]')
-botao_aplicar.click()
+clicar_elemento_com_espera((By.XPATH, '//*[@id="page-wrapper"]/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/div[5]/button[1]'))
 sleep(1)
 
 while True:
-
     excluir_anuncios(navegador)
-
 
     pag = navegador.page_source
     extra = BeautifulSoup(pag, 'html.parser')
@@ -77,7 +81,6 @@ while True:
     if pagination_ul:
         next_link = pagination_ul.find('li', class_='pnext')
         if next_link:
-            excluir_anuncios(navegador)
             print('rodando')
         else:
             excluir_anuncios(navegador)
